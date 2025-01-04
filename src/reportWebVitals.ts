5855a19dcca7095f7fd9032aceb018f6db2460bf
Ryoichi-Okubo-develop <ryoichi.okubo5@gmail.com>
2025-01-04 12:52:37 +0900

    initial commit

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,15 @@
+import { MetricType } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry?: (metric: MetricType) => void) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
+      onINP(onPerfEntry);
+      onCLS(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
