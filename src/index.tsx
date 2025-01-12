48b7fc01d1efc4b464abc4662ea619fc2e5d4963
Ryoichi-Okubo-develop <ryoichi.okubo5@gmail.com>
2025-01-12 12:51:23 +0900

    study of components

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import Hello from './components/Hello';
+// import Name from './components/Name';
+// import Message from './components/Message';
+import Parent from './components/ContainerSample';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     {/* <App /> */}
+//   </React.StrictMode>
+// );
 
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <Parent />
+    </React.StrictMode>
+  );
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
